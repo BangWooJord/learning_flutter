f0ad9f07586edf262cd4e0ebee14e4b0cbdc7e07
BangWooJord <58793833+BangWooJord@users.noreply.github.com>
2021-01-31 20:26:24 +0200

    Built an app for mertole measurement purposes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 void main() {
   runApp(MyApp());
 }
@@ -29,11 +29,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  double _ratio;
+  int pp;
+  double height;
+  void _changeHeight(double h){
+      height = h;
+  }
+  void _changePP(int p){
+      pp = p;
+  }
+  void _calcRatio() {
     setState(() {
-      _counter++;
+      _ratio =  100 * pp / height;
     });
   }
 
@@ -47,25 +54,80 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Button has been pressed',
+          children: <Widget>[TextField(
+              onChanged: (pp){
+                _changePP(int.parse(pp));
+              },
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+              textAlignVertical: TextAlignVertical.center,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.tealAccent[400],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                  labelText: 'Your gorgeous cock(cm)',
+                    hintText: '4',
+                    hintStyle: TextStyle(
+                      color: Colors.amberAccent[400]
+                  )
+              ),
             ),
+            SizedBox(height: 20, width: 20),
+            TextField(
+              onChanged: (height){
+                _changeHeight(double.parse(height));
+                _calcRatio();
+              },
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+              textAlignVertical: TextAlignVertical.center,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.tealAccent[400],
+              ),
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                labelText: 'Your marvelous height(cm)',
+                  hintText: '168',
+                  hintStyle: TextStyle(
+                      color: Colors.amberAccent[400]
+                  )
+              ),
+            ),
+            SizedBox(height: 60, width: 120),
             Text(
-              'times',
-            )
+              'Your height to cock ratio is',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                color: Colors.redAccent[700],
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '$_ratio %',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 40,
+                color: Colors.redAccent[700],
+              ),
+            ),
+            SizedBox(height: 50),
+            Image.asset('assets/images/floppa.jpeg'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
